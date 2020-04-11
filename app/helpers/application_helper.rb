a05c45ed2277803916b32a1238f18aabf7b65d5c
kelly r maass <kmaass77627@gmail.com>
2020-04-11 21:47:00 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def display_artist(name)
-    if @song.artist_name=(name)
-      <%= link_to name, artist_path(artist) %>
+  def display_artist(song)
+    if song.artist 
+      <%= link_to song.artist.name, song.artist %>
     else
-      <%= link_to 'Add Artist', song_path(song) %>
+      <%= link_to 'Add Artist', edit_song_path(song) %>
 
   end
 end
